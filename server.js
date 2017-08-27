d385d6407587a8ffd4b5ac4cb14617ac6986642a
Pandiselvisankar <30656211+Pandiselvisankar@users.noreply.github.com>
2017-08-27 10:46:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/artical-one',function (req,res) {
-  res.sendFile("article of one");
+  res.send("article of one");
 });
 
 app.get('/ui/style.css', function (req, res) {
